e4f398d2f4430721ce48bd7466ea5937e7e95116
renanns <atomsfrontenf@gmail.com>
2024-06-04 19:19:51 -0300

    feat: initial project config server

---
+++ server.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// Criando conexão com o banco de dados mongoose.
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.emit('pronto');
+  })
+  .catch(e => console.log(e));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+const routes = require('./routes');
+
+const path = require('path');
+
+const helmet = require('helmet');
+const csrf = require('csurf');
+
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+
+app.use(helmet());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Resolvendo arquivos estáticos.
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// Usando sessions para salvar os dados no navegador.
+const sessionOptions = session({
+  secret: 'hfdshfsjhfyrye343hjfhdu3443uhfhdj',
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  // Duração do cookie
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+});
+app.use(sessionOptions);
+// Menssagems para serem enviadas e logo após deixarem de existir.
+app.use(flash());
+
+// Config views e engine view
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Segurança de formulário
+app.use(csrf());
+
+// Middleware Globais para segurança.
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+app.use(routes);
+
+// Só inicia o servidor quando a promise da conexão com o banco emitir o sinal 'pronto'.
+app.on('pronto', () => {
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });
+});
